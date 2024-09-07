cee3b4337ab187bd7e66ec236c4a8f2884295f01
Alexey Guliakevich <gu_alexey@mail.ru>
2024-09-07 23:20:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from person import Person
+os.system('cls')
+
+list = []
+
+
+def info_list(list):
+    for i in list:
+        i.get_info()
+
+def append_person(list):
+    name = input("Name: ")
+    age = int(input("age: "))
+    list.append(Person(name,age))
+
+
+while(True):
+    os.system('cls')
+    print("\t\t\t\tmeny\n1. Добавить\n2. Просмотр\n3. Выход")
+    meny = int(input())
+    if meny == 1:
+        os.system('cls')
+        append_person(list)
+        input()
+    elif meny == 2:
+        os.system('cls')
+        info_list(list)
+        input()
+    elif meny==3: break
+
